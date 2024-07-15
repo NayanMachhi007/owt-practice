c4462a7528d9c5b4b0ef9527776ce8b0294af6a3
Nayan Machhi <nayan.machhi@idx.inc>
2024-07-15 06:26:38 +0530

    first commit

---
+++ javascript/find-largest-value-from-three-value.js
@@ -0,0 +1,13 @@
+let num1 = "2232343243434";
+let num2 = "5645";
+
+if(((num1[num1.length-1]) == 5 )  && ((num2[num2.length-1]) == 5)){
+    console.log("5");
+}
+else{
+console.log("not 5");
+}
+
+// // console.log(num1[num1.length  - 1]);
+
+
