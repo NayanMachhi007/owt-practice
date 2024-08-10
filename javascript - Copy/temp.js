f05597dddc93708715da799206a274b6d1812846
Nayan Machhi <nayan.machhi@idx.inc>
2024-08-10 15:31:28 +0530

    new code update now

---
+++ javascript - Copy/temp.js
@@ -0,0 +1,7 @@
+let num1 = 9090992;
+let num2 = 47852;
+if (num1 % 10 == num2 % 10) {
+  console.log("numbershavethesamelastdigitwhichis", num1 % 10 , num2 % 10);
+} else {
+  console.log("numbersdon'thavethesamelastdigit");
+}
